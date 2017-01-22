5d9280651f98f8e63e68536a555f2333fc94318c
wsavd <tuve@rootfest.net>
2017-01-22 11:14:11 +0300

    routing

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var index = require('./routes/index');
+var about = require('./routes/about');
+var contact = require('./routes/contact');
+
+var port = process.env.PORT || 8000;
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use('/', index);
+app.use('/about', about);
+app.use('/contact', contact);
+app.use(function(req,res,next){
+  res.status(404);
+  res.render('404');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function() {
+  console.log('Node app is running on port ', port);
 });
